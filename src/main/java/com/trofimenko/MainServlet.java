640b3dca2a15ecf442493a43a87170c16abfdc9a
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-07 14:37:33 +0300

    добавил web.xml и метод doGet

--- src/main/java/com/trofimenko/MainServlet.java
+++ src/main/java/com/trofimenko/MainServlet.java
@@ -1,3 +1,5 @@
+package com.trofimenko;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,14 +12,23 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        log("Metod init!");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Service enter\n");
         super.service(req, resp);
+        resp.getWriter().write("Service exit\n");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Metod doGet\n");
     }
 
     @Override
     public void destroy() {
+        log("Metod destroy!");
     }
 }
