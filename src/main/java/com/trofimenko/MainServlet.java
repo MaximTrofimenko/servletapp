65931988ff865b5400e347355ab50f6bc207db3e
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-07 14:56:53 +0300

    добавил аннотацию @WebServlet

--- src/main/java/com/trofimenko/MainServlet.java
+++ src/main/java/com/trofimenko/MainServlet.java
@@ -2,11 +2,13 @@ package com.trofimenko;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/cool-servlet")
 public class MainServlet extends HttpServlet {
 
     @Override
